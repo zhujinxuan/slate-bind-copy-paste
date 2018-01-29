194d9707dc15d0a3fea89622ed502ab007fe6cfc
zhujinxuan <zhujinxuan@gmail.com>
2018-01-29 10:56:09 -0500

    Simple tested insertFragmentAtRange

---
+++ tests/insert-fragment-before-void-block/change.js
@@ -0,0 +1,10 @@
+import { Document, Range } from 'slate';
+
+export default function(plugin, change) {
+    const insertFragmentAtRange = plugin.insertFragmentAtRange.generate();
+    const { document } = change.value;
+    const fragment = Document.create({ nodes: document.nodes.first().nodes });
+    const cursorBlock = document.getDescendant('_cursor_');
+    const range = Range.create().collapseToStartOf(cursorBlock);
+    return insertFragmentAtRange(change, range, fragment);
+}
